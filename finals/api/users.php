f4f774e74a683828c7011a04c0898a849c8dc8f1
gurung88k <156258318+gurung88k@users.noreply.github.com>
2024-04-16 09:09:47 -0400

    Add files via upload

---
+++ finals/api/users.php
@@ -0,0 +1,35 @@
+<?php
+// connect
+include('../include/db.php');
+
+$username=null;
+if (!empty($_GET['USERNAME'])) {
+    $username = $_GET['USERNAME'];
+}
+$sql="SELECT * FROM examusers ORDER BY $username";
+
+
+if (!empty($username)) {
+    $sql = "SELECT * FROM examusers WHERE username = :username";
+}
+
+$cmd = $db->prepare($sql);
+
+if (!empty($username)) {
+    $cmd->bindParam(':username', $username, PDO::PARAM_STR, 50);
+}
+
+$cmd->execute();
+$users = $cmd->fetchAll(PDO::FETCH_ASSOC);
+
+if (empty($users)) {
+    echo '{ code: 404, response: "Not Found" }';
+}
+else {
+    // output the json
+    echo json_encode($users);
+}
+
+// disconnect
+$db = null;
+?>
